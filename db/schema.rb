2b88b4156d88a95bbb97a746e531d721e1d8471d
Marymoore Patterson <mpatterson47@gmail.com>
2018-10-25 02:56:59 +0000

    password column

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181025022932) do
+ActiveRecord::Schema.define(version: 20181025025550) do
 
   create_table "comments", force: :cascade do |t|
     t.integer  "user_id"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20181025022932) do
     t.string  "last_name"
     t.integer "age"
     t.string  "email"
+    t.string  "password_digest"
   end
 
   add_index "users", ["email"], name: "index_users_on_email"
